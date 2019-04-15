19bac957a510022fe627ac9ab893c8cb7dfb50a3
WhiteHat135 <whitehat135@gmail.com>
2019-04-15 19:00:44 +0300

    dots

---
+++ i3/off/lock+sleep.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# make screenshot
+scrot /tmp/screen.png
+
+# scale image
+convert -scale 10% -scale 1000% /tmp/screen.png /tmp/screenl.png
+
+# clean
+rm -f /tmp/screen.png
+
+# lock screen
+i3lock -i /tmp/screenl.png
+
+#sleep
+systemctl suspend
